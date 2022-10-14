2cdd4f07678c831e59ba4c6b9fb72323e1aa0ae7
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-10-14 23:42:51 +0300

    added drag&drop and choosing file from folder

---
+++ wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿function Play_audio() {
+    var file_input = document.getElementById('file');
+    console.log(file_input.files[0]);
+    var audio = new Audio(file_input.files);
+    audio.play();
+}
